7e06498ad4709a7a35810a347fe12c94f4086580
nitr0011 <bnyoussef11@gmail.com>
2022-09-20 14:47:52 +0100

    navbar

---
+++ BookStore-Project/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import React from 'react'
+import Nav from './components/Nav/Nav';
+import styles from './Nav.module.css';
+
+function App() {
+  return (
+    <>
+    <Nav />
+     <h1>Welcome to our <span>BookStore</span> :)</h1>
+     <p>Our BookStore features the latest titles and gives you the best online bookshoping experience.</p>
+    
+    </>
+    
+   
+    
+      
+
+
+  );
+}
+
+export default App
