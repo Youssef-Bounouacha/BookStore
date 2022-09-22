80592d4c9412189450aabe54a8e6d97cb93c1fbc
nitr0011 <bnyoussef11@gmail.com>
2022-09-22 16:57:20 +0100

    navigation

---
+++ BookStore-Project/src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+
+function Home() {
+  return (
+    <div>
+      
+        
+    </div>
+    
+  )
+}
+
+export default Home
